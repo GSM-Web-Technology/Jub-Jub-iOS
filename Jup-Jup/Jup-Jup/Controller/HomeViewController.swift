96728cc37d77fac9de4e5a475accbf64b9680c87
jo030225 <whwngur123@naver.com>
2021-01-07 23:39:55 +0900

    검색 화면 추가

--- Jup-Jup/Jup-Jup/Controller/HomeViewController.swift
+++ Jup-Jup/Jup-Jup/Controller/HomeViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var searchView: UIView! {
+//        didSet {
+//            searchView.layer.borderWidth = 2
+//        }
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    @IBAction func searchTextField(_ sender: UITextField) {
+        
     }
     
 
