4d7f4959160fb0e5fa75ee426f88da9d05ee79e2
jo030225 <whwngur123@naver.com>
2021-01-19 19:45:06 +0900

    Sign Up 개발 중

--- Jup-Jup/Jup-Jup/Controller/Login/SignUpViewController.swift
+++ Jup-Jup/Jup-Jup/Controller/Login/SignUpViewController.swift
@@ -9,27 +9,45 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var SignUpEmail: UITextField! {
+        didSet {
+            SignUpEmail.layer.cornerRadius = 5
+            SignUpEmail.layer.borderWidth = 1
+        }
+    }
+    @IBOutlet weak var SignUpPassword: UITextField! {
+        didSet {
+            SignUpPassword.layer.cornerRadius = 5
+            SignUpPassword.layer.borderWidth = 1
+        }
+    }
+    @IBOutlet weak var SignUpPasswordCheck: UITextField! {
+        didSet {
+            SignUpPasswordCheck.layer.cornerRadius = 5
+            SignUpPasswordCheck.layer.borderWidth = 1
+        }
+    }
+    @IBOutlet weak var SignUpName: UITextField! {
+        didSet {
+            SignUpName.layer.cornerRadius = 5
+            SignUpName.layer.borderWidth = 1
+        }
+    }
     @IBOutlet weak var signUpClassNumber: UITextField! {
         didSet {
             signUpClassNumber.layer.cornerRadius = 5
             signUpClassNumber.layer.borderWidth = 1
         }
     }
+    @IBOutlet weak var signUpBtn: UIButton! {
+        didSet {
+            signUpBtn.layer.cornerRadius = 5
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
