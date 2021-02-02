0271c603b6a80d85ba2412ea3e6e45df67df224e
jo030225 <whwngur123@naver.com>
2021-02-02 22:00:27 +0900

    검색 기능 추가

--- Jup-Jup/Jup-Jup/Controller/Main/Home/HomeViewController.swift
+++ Jup-Jup/Jup-Jup/Controller/Main/Home/HomeViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class HomeViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var homeTableView: UITableView! {
         didSet {
             homeTableView.delegate = self
@@ -18,6 +18,17 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
     }
     @IBOutlet weak var searchTextField: UITextField!
     
+    var arr = ["DC모터", "한성노트북", "삼성노트북", "모니터", "아두이노", "iPad", "갤럭시탭", "키보드", "마우스", "애플 매직마우스", "애플 매직키보드"]
+    
+    var filteredArr: [String] = []
+    
+    var isFiltering: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        return isActive && isSearchBarHasText
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,10 +36,10 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
     }
     
     func searchController() {
-        // navigationBar에 검색란 넣기
         let searchController = UISearchController(searchResultsController: nil)
         self.navigationItem.searchController = searchController
         self.navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchResultsUpdater = self
     }
     
@@ -40,16 +51,12 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
         searchTextField.resignFirstResponder()
         return true
     }
-    
-    @IBAction func searchTextField(_ sender: UITextField) {
-        
-    }
 }
 
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return self.isFiltering ? self.filteredArr.count : self.arr.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -58,6 +65,11 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeTableViewCell", for: indexPath) as! HomeTableViewCell
+        if self.isFiltering {
+            cell.homeTitleName.text = self.filteredArr[indexPath.row]
+        } else {
+            cell.homeTitleName.text = self.arr[indexPath.row]
+        }
         
         return cell
     }
@@ -65,7 +77,9 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 
 extension HomeViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        print(searchController.searchBar.text ?? "")
+        guard let text = searchController.searchBar.text?.lowercased() else { return }
+        self.filteredArr = self.arr.filter { $0.localizedCaseInsensitiveContains(text) }
+        self.homeTableView.reloadData()
     }
     
     
