89d2dc60512a0da8b4058c179cb1c05958a23526
jo030225 <whwngur123@naver.com>
2021-02-03 23:01:03 +0900

    로그인 기능 개발 중

--- Jup-Jup/Jup-Jup/Controller/Login/LoginViewController.swift
+++ Jup-Jup/Jup-Jup/Controller/Login/LoginViewController.swift
@@ -6,11 +6,12 @@
 //
 
 import UIKit
-
+import Alamofire
 
 
 class LoginViewController: UIViewController {
     
+    var failMessages = "로그인 실패!!"
     
     @IBOutlet weak var logInEmail: UITextField! {
         didSet {
@@ -38,6 +39,12 @@ class LoginViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    func goMainPage(){
+        guard let goMain = self.storyboard?.instantiateViewController(identifier: "MainPage") else { return }
+        goMain.modalPresentationStyle = .fullScreen
+        self.present(goMain, animated: true)
+    }
+    
     func loginSucessAlert() {
         let alert = UIAlertController(title: "로그인 성공", message: "로그인 성공!!", preferredStyle: UIAlertController.Style.alert)
         let ok = UIAlertAction(title: "확인", style: UIAlertAction.Style.default) { (_) in
@@ -54,15 +61,54 @@ class LoginViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
     
-    @IBAction func logInButton(_ sender: UIButton) {
-       loginSucessAlert()
+    func checkTextField() -> Bool {
+        if (logInEmail.text == "") || (logInPassword.text == "") {
+            failMessages = "빈칸을 모두 입력해주세요!"
+            return false
+        }
+        
+        return true
     }
     
-    func goMainPage(){
-        guard let goMain = self.storyboard?.instantiateViewController(identifier: "MainPage") else { return }
-        goMain.modalPresentationStyle = .fullScreen
-        self.present(goMain, animated: true)
+    func signInApi(email: String,password: String) {
+        let URL = "http://3.36.29.69:8080/v1/signin"
+        
+        let PARAM: Parameters = [
+            "email": email,
+            "password": password
+        ]
+        
+        let alamo = AF.request(URL, method: .post, parameters: PARAM,encoding: JSONEncoding.default).validate(statusCode: 200..<300)
+        
+        
+        alamo.responseString() { response in
+            switch response.result
+            {
+            //통신성공
+            case .success(let value):
+                self.loginSucessAlert()
+                print("value: \(value)")
+            //통신실패
+            case .failure(let error):
+                
+                print("error: \(String(describing: error.errorDescription))")
+                print("\(error)")
+            }
+        }
+        
     }
+    
+    @IBAction func logInButton(_ sender: UIButton) {
+        if (checkTextField()) {
+            signInApi(email: logInEmail.text!, password: logInPassword.text!)
+        } else {
+            loginFailAlert()
+        }
+        
+        
+    }
+    
+    
 }
 
 extension LoginViewController: UITextFieldDelegate {
