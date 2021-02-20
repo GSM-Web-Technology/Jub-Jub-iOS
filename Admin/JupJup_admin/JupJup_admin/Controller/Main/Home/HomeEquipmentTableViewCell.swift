0b63087a321c10e9f3165fdb64d0b2084ba0fd3e
jo030225 <whwngur123@naver.com>
2021-02-21 01:33:18 +0900

    [ADD] admin 파일 추가

---
+++ Admin/JupJup_admin/JupJup_admin/Controller/Main/Home/HomeEquipmentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeEquipmentTableViewCell.swift
+//  JupJup_admin
+//
+//  Created by 조주혁 on 2021/02/21.
+//
+
+import UIKit
+
+class HomeEquipmentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Admin/JupJup_admin/JupJup_admin/Controller/Main/Home/HomeLaptopTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeLaptopTableViewCell.swift
+//  JupJup_admin
+//
+//  Created by 조주혁 on 2021/02/21.
+//
+
+import UIKit
+
+class HomeLaptopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
