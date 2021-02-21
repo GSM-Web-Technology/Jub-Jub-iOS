fdb6ba96b4b3a912052720eb2fd63065671bda48
jo030225 <whwngur123@naver.com>
2021-02-22 00:46:36 +0900

    [ADD] Manager 추가

--- User/Jup-Jup/Jup-Jup/Controller/Main/MyPage/MyPageViewController.swift
+++ User/Jup-Jup/Jup-Jup/Controller/Main/MyPage/MyPageViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 import Alamofire
 
+
+
+
 class MyPageViewController: UIViewController {
     
     var model: LogOutModel?
---
+++ User/Jup-Jup/Jup-Jup/Manager/TokenManager.swift
@@ -0,0 +1,8 @@
+//
+//  TokenManager.swift
+//  Jup-Jup
+//
+//  Created by 조주혁 on 2021/02/22.
+//
+
+import Foundation
