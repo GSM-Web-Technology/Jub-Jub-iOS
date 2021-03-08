0eb2adbab7ffd3ee7d5408b96a2e84cf933a5a11
jo030225 <whwngur123@naver.com>
2021-03-08 11:30:12 +0900

    [UPDATE] Admin Login 개발 중

--- Admin/JupJup_admin/JupJup_admin/Controller/Login/LoginViewController.swift
+++ Admin/JupJup_admin/JupJup_admin/Controller/Login/LoginViewController.swift
@@ -6,14 +6,92 @@
 //
 
 import UIKit
+import Alamofire
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var logInEmailTextField: UITextField!
-    @IBOutlet weak var logInPasswordTextField: UITextField!
+    @IBOutlet weak var logInEmailTextField: UITextField! {
+        didSet {
+            logInEmailTextField.layer.cornerRadius = 5
+            logInEmailTextField.layer.borderWidth = 1
+            logInEmailTextField.delegate = self
+        }
+    }
+    @IBOutlet weak var logInPasswordTextField: UITextField! {
+        didSet {
+            logInPasswordTextField.layer.cornerRadius = 5
+            logInPasswordTextField.layer.borderWidth = 1
+            logInPasswordTextField.delegate = self
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
+    
+    func goMainPage(){
+        guard let goMain = self.storyboard?.instantiateViewController(identifier: "MainPage") else { return }
+        goMain.modalPresentationStyle = .fullScreen
+        self.present(goMain, animated: true)
+    }
+    
+    func apiCall(email: String,password: String) {
+        let URL = "http://15.165.97.179:8080/v2/signin"
+        let PARAM: Parameters = [
+            "email": email,
+            "password": password
+        ]
+        AF.request(URL, method: .post, parameters: PARAM, encoding: JSONEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                if let dic = value as? NSDictionary {
+                    if let code = dic["code"] as? Int {
+                        switch code {
+                        case 0:
+                            if let allToken = dic["data"] as? NSDictionary {
+                                if let token = allToken["accessToken"] as? String {
+                                    print(token)
+                                    KeychainManager.saveToken(token: token)
+                                }
+                            }
+                            KeychainManager.saveEmail(email: self.logInEmail.text!)
+                            KeychainManager.savePassword(password: self.logInPassword.text!)
+                            self.loginSucessAlert()
+                        case -1001:
+                            self.loginFailAlert(messages: "계정이 존재하지 않거나 이메일 또는 비밀번호가 정확하지 않습니다.")
+                        case -947:
+                            self.loginFailAlert(messages: "이메일 인증을 해주세요!")
+                        default:
+                            return
+                        }
+                    }
+                }
+            case .failure(let e):
+                self.loginFailAlert(messages: "네트워크가 원활하지 않습니다.")
+                print(e.localizedDescription)
+            }
+        }
+    }
+    
+    func loginSucessAlert() {
+        let alert = UIAlertController(title: "로그인 성공", message: "로그인 성공!!", preferredStyle: UIAlertController.Style.alert)
+        let ok = UIAlertAction(title: "확인", style: UIAlertAction.Style.default) { (_) in
+            self.goMainPage()
+        }
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func loginFailAlert(messages: String) {
+        let alert = UIAlertController(title: "로그인 실패", message: messages, preferredStyle: UIAlertController.Style.alert)
+        let ok = UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
 
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
 }
