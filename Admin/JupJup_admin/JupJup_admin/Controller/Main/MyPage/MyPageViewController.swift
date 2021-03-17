117f8786d4a6baa2bd4c41b61bb9e3ba219aa967
jo030225 <whwngur123@naver.com>
2021-03-17 21:04:02 +0900

    [ADD] 로그아웃 기능 구현

--- Admin/JupJup_admin/JupJup_admin/Controller/Main/MyPage/MyPageViewController.swift
+++ Admin/JupJup_admin/JupJup_admin/Controller/Main/MyPage/MyPageViewController.swift
@@ -6,11 +6,68 @@
 //
 
 import UIKit
+import Alamofire
 
 class MyPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    func signOutApiCall() {
+        let URL = "http://15.165.97.179:8080/v2/logout"
+        let token = KeychainManager.getToken()
+        AF.request(URL, method: .post , headers: ["Authorization" : token]).responseJSON { (response) in
+            switch response.result {
+            case .success(let value):
+                if let dic = value as? NSDictionary {
+                    if let success = dic["success"] as? Bool {
+                        switch success {
+                        case true:
+                            KeychainManager.removeToken()
+                            KeychainManager.removeEmail()
+                            KeychainManager.removePassword()
+                            self.goLoginPage()
+                        default:
+                            self.logOutFailAlert()
+                        }
+                    }
+                }
+            case . failure(let error):
+                self.logOutFailAlert()
+                print("\(error.localizedDescription)")
+            }
+            
+        }
+    }
+    
+    func logOutAlert() {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let ok = UIAlertAction(title: "로그아웃", style: .destructive) { (_) in
+            self.signOutApiCall()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func logOutFailAlert() {
+        let alert = UIAlertController(title: "로그아웃 실패!", message: "네트워크가 원활하지 않습니다.", preferredStyle: UIAlertController.Style.alert)
+        let ok = UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func goLoginPage(){
+        guard let goMain = self.storyboard?.instantiateViewController(identifier: "LoginViewController") else { return }
+        goMain.modalPresentationStyle = .fullScreen
+        self.present(goMain, animated: true)
+    }
+    
+    @IBAction func signOutButton(_ sender: Any) {
+        logOutAlert()
     }
 }
