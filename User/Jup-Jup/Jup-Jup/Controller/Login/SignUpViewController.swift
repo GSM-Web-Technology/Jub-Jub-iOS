410ba8061b2bfac50c36a066c97610e484fe21ad
jo030225 <whwngur123@naver.com>
2021-03-18 15:47:12 +0900

    [UPDATE] 스플래쉬, 로그인, 회원가입 화면 디자인 수정

--- User/Jup-Jup/Jup-Jup/Controller/Login/SignUpViewController.swift
+++ User/Jup-Jup/Jup-Jup/Controller/Login/SignUpViewController.swift
@@ -16,50 +16,34 @@ class SignUpViewController: UIViewController {
     
     @IBOutlet weak var signUpEmail: UITextField! {
         didSet {
-            signUpEmail.layer.cornerRadius = 5
-            signUpEmail.layer.borderWidth = 1
             signUpEmail.delegate = self
         }
     }
     @IBOutlet weak var signUpPassword: UITextField! {
         didSet {
-            signUpPassword.layer.cornerRadius = 5
-            signUpPassword.layer.borderWidth = 1
             signUpPassword.delegate = self
         }
     }
     @IBOutlet weak var signUpPasswordCheck: UITextField! {
         didSet {
-            signUpPasswordCheck.layer.cornerRadius = 5
-            signUpPasswordCheck.layer.borderWidth = 1
             signUpPasswordCheck.delegate = self
         }
     }
     @IBOutlet weak var signUpName: UITextField! {
         didSet {
-            signUpName.layer.cornerRadius = 5
-            signUpName.layer.borderWidth = 1
             signUpName.delegate = self
         }
     }
     @IBOutlet weak var signUpClassNumber: UITextField! {
         didSet {
-            signUpClassNumber.layer.cornerRadius = 5
-            signUpClassNumber.layer.borderWidth = 1
             signUpClassNumber.delegate = self
         }
     }
     @IBOutlet weak var signUpBtn: UIButton! {
         didSet {
-            signUpBtn.layer.cornerRadius = 5
+            signUpBtn.layer.cornerRadius = 10
         }
     }
-    @IBOutlet weak var backBtn: UIButton! {
-        didSet {
-            backBtn.layer.cornerRadius = 5
-        }
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         indicatorAutolayout()
